dc558c1cb12bdea0e6eaea7d9cdaa8bf1a850374
ForeverW <402733119@qq.com>
2018-11-19 17:46:09 +0800

    Pop Or Push Fucntion

--- 3.1.c
+++ 3.1.c
@@ -8,8 +8,8 @@
 #define ERROR 0
 #define OVERFLOW -2
 
-#define INIT_SIZE = 10;
-#define INCREMENT_SIZE = 5;
+#define INIT_SIZE  10
+#define INCREMENT_SIZE  5
 
 typedef int Elemtype;
 typedef int Status;
@@ -35,19 +35,30 @@ Status InitStack(SqStack *S)
 
 Status Push(SqStack *S, Elemtype e)
 {
-    if(S->top >= S->size)
+    if((S->top - S->base) / sizeof(Elemtype) >= S->size)
     {
        S->base = (Elemtype*)realloc(S->base, (S->size + INCREMENT_SIZE)*sizeof(Elemtype));
        if(!S->base)
        {
            exit(OVERFLOW);
        }
+       S->top = S->base + S->size;
+       S->size += INCREMENT_SIZE;
     }
-    S->top = S->base + S->size;
-    S->size += INCREMENT_SIZE;
 
-    *S->top = e;
+    *(S->top) = e;
     S->top += sizeof(Elemtype);
+    return OK;
+}
+
+Status Pop(SqStack *S, Elemtype *e)
+{
+    if(S->top <= S->base)
+    {
+        exit(OVERFLOW);
+    }
+    S->top -= sizeof(Elemtype);
+   *e = *(S->top);
     return OK;
 }
 
@@ -64,5 +75,27 @@ void TraverseList(SqStack S, void(*visit)(Elemtype))
         visit(*S.base);
         S.base += sizeof(Elemtype);
     }
+}
+
+int main()
+{
+    int i;
+    Elemtype e = 0;
+    SqStack S;
+    InitStack(&S);
+
+    printf("Push Function\n");
+    for(i = 0 ; i<5 ; i++)
+    {
+        Push(&S,i*10);
+    }
+
+    for(i = 0; i<4; i++)
+    {
+        Pop(&S,&e);
+        printf("The %dth elem is: %d \n",i,e);
+    }
+
     return OK;
+
 }
